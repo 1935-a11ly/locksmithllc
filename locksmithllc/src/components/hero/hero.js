ef87aad2bab774db148c43e10f42d85796d18e26
Michael Taona Keith Musa <78579318+1935-a11ly@users.noreply.github.com>
2024-03-27 21:29:04 -0500

    heroupdates

--- locksmithllc/src/components/hero/hero.js
+++ locksmithllc/src/components/hero/hero.js
@@ -4,8 +4,12 @@ import '../../App.css';
 const Hero = () => {
     return (
         <div className='herosection'>
-            <div className='float-left pt-[50px] text-white px-24'>
-                <video src="./hero.mp4" autoPlay muted loop className='w-[600px] h-fit rounded-3xl opacity-[40%] mb-16 mt-16'/>  
+            {/* <div className='flex justify-center items-center mb-2 pt-6'>
+            <img src='./ylogo.png' className='w-[130px] h-[130px] rounded-full opacity-[50%]'></img>
+            </div>
+            <div><h1 className='pt-[0px] text-center block text-3xl text-white font-light mb-6'>One Stop For All Your Lock-Out Needs.</h1></div> */}
+            <div className=' pt-[80px] text-white px-24 block'>
+                <video src="./hero.mp4" autoPlay muted loop className='w-[850px] h-fit rounded-3xl opacity-[60%] mb-16 mt-0'/>  
                 <h1 className='text-5xl'>PROFESSIONAL LOCKSMITH SERVICES</h1> 
                 <h2 className='text-[16px] pt-8 pb-8'>Providing reliable and professional locksmith services for all your lock and key needs 24/7</h2>
                 <div className='space-x-6'>
@@ -18,7 +22,16 @@ const Hero = () => {
                 <button className='bg-[#48341c] rounded-xl text-white p-[11px]'>Contact Us</button>
                 </div>
             </div>
+            <div className='flex justify-end items-end mt-8 px-24'>
+                <h2 className='text-white block opacity-[80%]'>One Stop For All Your Lockout Needs.</h2>
+            </div>
+            <div className='flex justify-end items-end px-24'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="text-[#fbd8a0] w-12 h-12 mt-4 animate-pulse block">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z" />
+                </svg>
+            </div>
         </div>
+        
     );
 };
 
