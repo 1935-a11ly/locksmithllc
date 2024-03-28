d182eec536863dffd3def1b5b0df92c24e492479
Michael Taona Keith Musa <78579318+1935-a11ly@users.noreply.github.com>
2024-03-28 05:52:22 -0500

    infinite carousal update and general UI bug fixes

--- locksmithllc/src/components/testimonials/testimonials.js
+++ locksmithllc/src/components/testimonials/testimonials.js
@@ -3,15 +3,272 @@ import '../../App.css';
 
 const Testimonials = () => {
     return (
+        <>
         <div className='bg-[#130a07] w-full h-fit'>
             <div className='text-white text-center pt-12'>
                 <div className='flex justify-center align-center pb-8'>
                     <img src='./ylogo.png' className='  w-[120px] h-[120px] rounded-full'></img>
                 </div>
-                <h1 className='text-5xl px-[250px]'>Reach Out to Us for Expert Locksmith Services</h1>
+                <h1 className='text-5xl'>Reach Out to Us for Expert Locksmith Services</h1>
                 <h3 className='text-lg pt-8 pb-20 opacity-[60%]'> Need help with your locks? Don't hesitate to contact us. Our team of expert locksmiths is always ready to assist you</h3>
             </div>
-        </div>   
+        </div>
+        <div className='pb-28'>
+            <div className='text-center pt-8'>
+                <h3 className='text-lg pt-20 text-[#48341c]'> Testimonials </h3>
+                <h1 className='text-5xl pt-8 pb-20'>Voices of Satisfied Customers</h1>
+            </div>
+            <div className="slider2 bg-transparent w-full ">
+                <div className="slide-track2 bg-transparent">
+
+
+
+
+                    <div className="slide2 mx-20">
+                        <div className="shadow-md w-[440px] h-[120px] flex flex-col justify-center gap-2 bg-transparent rounded-xl p-2 backdrop-blur-sm ">
+                            <div className="flex gap-2">
+                                <img alt="" src= './reactlogo.png' className="bg-base-300 w-16 h-16 shrink-0 rounded-full object-contain mt-[19px]" />
+                                <div className="flex flex-col text-white">
+                                    <span className="text-sm text-black px-2 pt-4 opacity-[80%]">"Exceptional locksmith service! Quick, reliable, and professional."</span>
+                                    <p className="line-clamp-3 font-bold text-[#48341c] text-[18px] bg-base-300 rounded-lg p-2">
+                                        Rob M.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="slide2 mx-20">
+                        <div className="shadow-md w-[440px] h-[120px] flex flex-col justify-center gap-2 bg-transparent rounded-xl p-2 backdrop-blur-sm ">
+                            <div className="flex gap-2">
+                                <img alt="" src= './reactlogo.png' className="bg-base-300 w-16 h-16 shrink-0 rounded-full object-contain mt-[19px]" />
+                                <div className="flex flex-col text-white">
+                                    <span className="text-sm text-black px-2 pt-4 opacity-[80%]">"Exceptional locksmith service! Quick, reliable, and professional."</span>
+                                    <p className="line-clamp-3 font-bold text-[#48341c] text-[18px] bg-base-300 rounded-lg p-2">
+                                        Rob M.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="slide2 mx-20">
+                        <div className="shadow-md w-[440px] h-[120px] flex flex-col justify-center gap-2 bg-transparent rounded-xl p-2 backdrop-blur-sm ">
+                            <div className="flex gap-2">
+                                <img alt="" src= './reactlogo.png' className="bg-base-300 w-16 h-16 shrink-0 rounded-full object-contain mt-[19px]" />
+                                <div className="flex flex-col text-white">
+                                    <span className="text-sm text-black px-2 pt-4 opacity-[80%]">"Exceptional locksmith service! Quick, reliable, and professional."</span>
+                                    <p className="line-clamp-3 font-bold text-[#48341c] text-[18px] bg-base-300 rounded-lg p-2">
+                                        Rob M.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="slide2 mx-20">
+                        <div className="shadow-md w-[440px] h-[120px] flex flex-col justify-center gap-2 bg-transparent rounded-xl p-2 backdrop-blur-sm ">
+                            <div className="flex gap-2">
+                                <img alt="" src= './reactlogo.png' className="bg-base-300 w-16 h-16 shrink-0 rounded-full object-contain mt-[19px]" />
+                                <div className="flex flex-col text-white">
+                                    <span className="text-sm text-black px-2 pt-4 opacity-[80%]">"Exceptional locksmith service! Quick, reliable, and professional."</span>
+                                    <p className="line-clamp-3 font-bold text-[#48341c] text-[18px] bg-base-300 rounded-lg p-2">
+                                        Rob M.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="slide2 mx-20">
+                        <div className="shadow-md w-[440px] h-[120px] flex flex-col justify-center gap-2 bg-transparent rounded-xl p-2 backdrop-blur-sm ">
+                            <div className="flex gap-2">
+                                <img alt="" src= './reactlogo.png' className="bg-base-300 w-16 h-16 shrink-0 rounded-full object-contain mt-[19px]" />
+                                <div className="flex flex-col text-white">
+                                    <span className="text-sm text-black px-2 pt-4 opacity-[80%]">"Exceptional locksmith service! Quick, reliable, and professional."</span>
+                                    <p className="line-clamp-3 font-bold text-[#48341c] text-[18px] bg-base-300 rounded-lg p-2">
+                                        Rob M.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="slide2 mx-20">
+                        <div className="shadow-md w-[440px] h-[120px] flex flex-col justify-center gap-2 bg-transparent rounded-xl p-2 backdrop-blur-sm ">
+                            <div className="flex gap-2">
+                                <img alt="" src= './reactlogo.png' className="bg-base-300 w-16 h-16 shrink-0 rounded-full object-contain mt-[19px]" />
+                                <div className="flex flex-col text-white">
+                                    <span className="text-sm text-black px-2 pt-4 opacity-[80%]">"Exceptional locksmith service! Quick, reliable, and professional."</span>
+                                    <p className="line-clamp-3 font-bold text-[#48341c] text-[18px] bg-base-300 rounded-lg p-2">
+                                        Rob M.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="slide2 mx-20">
+                        <div className="shadow-md w-[440px] h-[120px] flex flex-col justify-center gap-2 bg-transparent rounded-xl p-2 backdrop-blur-sm ">
+                            <div className="flex gap-2">
+                                <img alt="" src= './reactlogo.png' className="bg-base-300 w-16 h-16 shrink-0 rounded-full object-contain mt-[19px]" />
+                                <div className="flex flex-col text-white">
+                                    <span className="text-sm text-black px-2 pt-4 opacity-[80%]">"Exceptional locksmith service! Quick, reliable, and professional."</span>
+                                    <p className="line-clamp-3 font-bold text-[#48341c] text-[18px] bg-base-300 rounded-lg p-2">
+                                        Rob M.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="slide2 mx-20">
+                        <div className="shadow-md w-[440px] h-[120px] flex flex-col justify-center gap-2 bg-transparent rounded-xl p-2 backdrop-blur-sm ">
+                            <div className="flex gap-2">
+                                <img alt="" src= './reactlogo.png' className="bg-base-300 w-16 h-16 shrink-0 rounded-full object-contain mt-[19px]" />
+                                <div className="flex flex-col text-white">
+                                    <span className="text-sm text-black px-2 pt-4 opacity-[80%]">"Exceptional locksmith service! Quick, reliable, and professional."</span>
+                                    <p className="line-clamp-3 font-bold text-[#48341c] text-[18px] bg-base-300 rounded-lg p-2">
+                                        Rob M.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="slide2 mx-20">
+                        <div className="shadow-md w-[440px] h-[120px] flex flex-col justify-center gap-2 bg-transparent rounded-xl p-2 backdrop-blur-sm ">
+                            <div className="flex gap-2">
+                                <img alt="" src= './reactlogo.png' className="bg-base-300 w-16 h-16 shrink-0 rounded-full object-contain mt-[19px]" />
+                                <div className="flex flex-col text-white">
+                                    <span className="text-sm text-black px-2 pt-4 opacity-[80%]">"Exceptional locksmith service! Quick, reliable, and professional."</span>
+                                    <p className="line-clamp-3 font-bold text-[#48341c] text-[18px] bg-base-300 rounded-lg p-2">
+                                        Rob M.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="slide2 mx-20">
+                        <div className="shadow-md w-[440px] h-[120px] flex flex-col justify-center gap-2 bg-transparent rounded-xl p-2 backdrop-blur-sm ">
+                            <div className="flex gap-2">
+                                <img alt="" src= './reactlogo.png' className="bg-base-300 w-16 h-16 shrink-0 rounded-full object-contain mt-[19px]" />
+                                <div className="flex flex-col text-white">
+                                    <span className="text-sm text-black px-2 pt-4 opacity-[80%]">"Exceptional locksmith service! Quick, reliable, and professional."</span>
+                                    <p className="line-clamp-3 font-bold text-[#48341c] text-[18px] bg-base-300 rounded-lg p-2">
+                                        Rob M.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="slide2 mx-20">
+                        <div className="shadow-md w-[440px] h-[120px] flex flex-col justify-center gap-2 bg-transparent rounded-xl p-2 backdrop-blur-sm ">
+                            <div className="flex gap-2">
+                                <img alt="" src= './reactlogo.png' className="bg-base-300 w-16 h-16 shrink-0 rounded-full object-contain mt-[19px]" />
+                                <div className="flex flex-col text-white">
+                                    <span className="text-sm text-black px-2 pt-4 opacity-[80%]">"Exceptional locksmith service! Quick, reliable, and professional."</span>
+                                    <p className="line-clamp-3 font-bold text-[#48341c] text-[18px] bg-base-300 rounded-lg p-2">
+                                        Rob M.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="slide2 mx-20">
+                        <div className="shadow-md w-[440px] h-[120px] flex flex-col justify-center gap-2 bg-transparent rounded-xl p-2 backdrop-blur-sm ">
+                            <div className="flex gap-2">
+                                <img alt="" src= './reactlogo.png' className="bg-base-300 w-16 h-16 shrink-0 rounded-full object-contain mt-[19px]" />
+                                <div className="flex flex-col text-white">
+                                    <span className="text-sm text-black px-2 pt-4 opacity-[80%]">"Exceptional locksmith service! Quick, reliable, and professional."</span>
+                                    <p className="line-clamp-3 font-bold text-[#48341c] text-[18px] bg-base-300 rounded-lg p-2">
+                                        Rob M.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="slide2 mx-20">
+                        <div className="shadow-md w-[440px] h-[120px] flex flex-col justify-center gap-2 bg-transparent rounded-xl p-2 backdrop-blur-sm ">
+                            <div className="flex gap-2">
+                                <img alt="" src= './reactlogo.png' className="bg-base-300 w-16 h-16 shrink-0 rounded-full object-contain mt-[19px]" />
+                                <div className="flex flex-col text-white">
+                                    <span className="text-sm text-black px-2 pt-4 opacity-[80%]">"Exceptional locksmith service! Quick, reliable, and professional."</span>
+                                    <p className="line-clamp-3 font-bold text-[#48341c] text-[18px] bg-base-300 rounded-lg p-2">
+                                        Rob M.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="slide2 mx-20">
+                        <div className="shadow-md w-[440px] h-[120px] flex flex-col justify-center gap-2 bg-transparent rounded-xl p-2 backdrop-blur-sm ">
+                            <div className="flex gap-2">
+                                <img alt="" src= './reactlogo.png' className="bg-base-300 w-16 h-16 shrink-0 rounded-full object-contain mt-[19px]" />
+                                <div className="flex flex-col text-white">
+                                    <span className="text-sm text-black px-2 pt-4 opacity-[80%]">"Exceptional locksmith service! Quick, reliable, and professional."</span>
+                                    <p className="line-clamp-3 font-bold text-[#48341c] text-[18px] bg-base-300 rounded-lg p-2">
+                                        Rob M.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="slide2 mx-20">
+                        <div className="shadow-md w-[440px] h-[120px] flex flex-col justify-center gap-2 bg-transparent rounded-xl p-2 backdrop-blur-sm ">
+                            <div className="flex gap-2">
+                                <img alt="" src= './reactlogo.png' className="bg-base-300 w-16 h-16 shrink-0 rounded-full object-contain mt-[19px]" />
+                                <div className="flex flex-col text-white">
+                                    <span className="text-sm text-black px-2 pt-4 opacity-[80%]">"Exceptional locksmith service! Quick, reliable, and professional."</span>
+                                    <p className="line-clamp-3 font-bold text-[#48341c] text-[18px] bg-base-300 rounded-lg p-2">
+                                        Rob M.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="slide2 mx-20">
+                        <div className="shadow-md w-[440px] h-[120px] flex flex-col justify-center gap-2 bg-transparent rounded-xl p-2 backdrop-blur-sm ">
+                            <div className="flex gap-2">
+                                <img alt="" src= './reactlogo.png' className="bg-base-300 w-16 h-16 shrink-0 rounded-full object-contain mt-[19px]" />
+                                <div className="flex flex-col text-white">
+                                    <span className="text-sm text-black px-2 pt-4 opacity-[80%]">"Exceptional locksmith service! Quick, reliable, and professional."</span>
+                                    <p className="line-clamp-3 font-bold text-[#48341c] text-[18px] bg-base-300 rounded-lg p-2">
+                                        Rob M.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="slide2 mx-20">
+                        <div className="shadow-md w-[440px] h-[120px] flex flex-col justify-center gap-2 bg-transparent rounded-xl p-2 backdrop-blur-sm ">
+                            <div className="flex gap-2">
+                                <img alt="" src= './reactlogo.png' className="bg-base-300 w-16 h-16 shrink-0 rounded-full object-contain mt-[19px]" />
+                                <div className="flex flex-col text-white">
+                                    <span className="text-sm text-black px-2 pt-4 opacity-[80%]">"Exceptional locksmith service! Quick, reliable, and professional."</span>
+                                    <p className="line-clamp-3 font-bold text-[#48341c] text-[18px] bg-base-300 rounded-lg p-2">
+                                        Rob M.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </div>
+        </div>
+
+        </> 
     );
 };
 
