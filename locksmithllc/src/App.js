e78dbb36c93c91d3d740008f0d92112ce5e14ab2
Michael Taona Keith Musa <78579318+1935-a11ly@users.noreply.github.com>
2024-04-10 03:39:29 -0500

    about section

--- locksmithllc/src/App.js
+++ locksmithllc/src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar/navbar.js';
+import About from './components/about/about.js';
 import Testimonials from './components/testimonials/testimonials.js';
 import Contact from './components/contact/contact.js';
 import Footer from './components/footer/footer.js';
 import Hero from './components/hero/hero.js';
 import Services from './components/services/services.js';
 
+
 function App() {
   return (
-    <div className="">
-      <Navbar/>
-      <Hero/>
-      <Services/>
-      <Testimonials/>
-      <Contact/>
-      <Footer/>
-    </div>
+      <>
+        <Navbar />
+        <Hero />
+        <About />
+        <Services />
+        <Testimonials />
+        <Contact />
+        <Footer />
+      </>
   );
 }
 
---
+++ locksmithllc/src/components/about/about.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="bg-gray-100 py-6 px-4 sm:px-6 lg:px-8 pb-32 pt-16">
+        <div className="max-w-7xl mx-auto">
+            <h2 className="text-3xl font-bold text-center mb-6 pb-12">About Us</h2>
+            <div className="flex flex-col-reverse md:flex-row items-center justify-center">
+                <div className="md:w-1/2 mb-6 md:mb-0">
+                    <img className="rounded-md shadow-md mx-auto md:mx-0" src="/locksmith-team.jpg" alt="Locksmith Team" />
+                </div>
+                <div className="md:w-1/2">
+                    <p className="text-lg mb-4">
+                        At Yemanis Locksmith, we take pride in our craft and commitment to serving the Houston community.
+                        Established over 5 years ago, we have grown to become a trusted name in the locksmith industry.
+                    </p>
+                    <p className="text-lg mb-4">
+                        With a dedicated team of professionals, we have successfully served over 200 clients,
+                        providing prompt and reliable locksmith services tailored to their needs.
+                    </p>
+                    <p className="text-lg mb-4">
+                        Whether you're locked out of your home, need to upgrade your business security,
+                        or require emergency assistance, you can count on us to deliver exceptional services
+                        with a human touch.
+                    </p>
+                    <p className="text-lg">
+                        Our mission is simple: to ensure the safety and security of our customers,
+                        providing peace of mind in every lock and key solution we deliver.
+                    </p>
+                </div>
+            </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
